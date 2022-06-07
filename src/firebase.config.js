fe442a9e02545bb4aebfa74ff5d5d2026c2d194c
Dilla Hani <hanidilla8@gmail.com>
2022-06-07 20:18:49 +0700

    firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyA8zoIRgwfKMwCtTlxDXHTzCBJOWOemfyI",
+    authDomain: "fir-5c745.firebaseapp.com",
+    projectId: "fir-5c745",
+    storageBucket: "fir-5c745.appspot.com",
+    messagingSenderId: "546461332116",
+    appId: "1:546461332116:web:d7682775c10d26146d3a81"
+  };
